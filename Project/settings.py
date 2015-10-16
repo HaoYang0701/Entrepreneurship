44867eb51af8c22a60fc6220290d19f5dac0c2ef
haoyang0701 <yang.hao0701@gmail.com>
2015-10-15 23:19:51 -0400

    deploy

--- Project/settings.py
+++ Project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'o@p*0_5ix9qdn7o@v*l$!q2c*zm-wo#jny9ofl*9147ch3278v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
