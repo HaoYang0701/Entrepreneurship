dba8172e45e18a320254650da4db24456e89f8a5
haoyang0701 <yang.hao0701@gmail.com>
2015-10-15 20:16:25 -0400

    plss

--- Project/settings.py
+++ Project/settings.py
@@ -75,12 +75,8 @@ WSGI_APPLICATION = 'Project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+
+DATABASES['default'] =  dj_database_url.config()
 
 
 # Internationalization
